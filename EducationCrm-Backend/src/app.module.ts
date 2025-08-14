9ee545027733cabc01718460e41700d0f35cd562
unawarexi <unawarexi@gmail.com>
2025-08-14 16:30:43 +0100

    App modules, service and controller

--- EducationCrm-Backend/src/app.module.ts
+++ EducationCrm-Backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
+import { AssignmentController } from './assignment/assignment.controller';
+import { AssignmentService } from './assignment/assignment.service';
+import { AssignmentModule } from './assignment/assignment.module';
+import { EnrollmentModule } from './enrollment/enrollment.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, AuthModule, UserModule, CourseModule, AssignmentModule, EnrollmentModule],
+  controllers: [AppController, UserController, AssignmentController],
+  providers: [AppService, UserService, AssignmentService],
 })
 export class AppModule {}
---
+++ EducationCrm-Backend/src/assignment/assignment.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('assignment')
+export class AssignmentController {}
